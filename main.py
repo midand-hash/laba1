63387a1692ed6230fbef52865d8a9a62ac13bb86
midand-hash <midovandik07@gmail.com>
2024-12-20 12:28:51 +0300

    Заменил старый код на новый

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-import time
-start_time= time.time()
-def fun():
-    a=2
-    b=3
-    c=a+b
-end_time= time.time()
-fun()
-timetaken = end_time - start_time
-print("Your program takes: ", timetaken) # 0.0345
+import random
+
+
+def guess_the_number():
+    number_to_guess = random.randint(1, 50)
+    attempts = 0
+
+    print("Я загадал число от 1 до 100. Попробуй угадать!")
+
+    while True:
+        user_guess = input("Введите ваше число: ")
+
+        # Проверяем, является ли ввод числом
+        try:
+            user_guess = int(user_guess)
+            attempts += 1
+        except ValueError:
+            print("Пожалуйста, введите целое число.")
+            continue
+
+        if user_guess < number_to_guess:
+            print("Слишком низко, попробуйте еще раз.")
+        elif user_guess > number_to_guess:
+            print("Слишком высоко, попробуйте еще раз.")
+        else:
+            print(f"Вы угадали! Загаданное число: {number_to_guess}. Попытки: {attempts}")
+            break
+
+
+guess_the_number()
